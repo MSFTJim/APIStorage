7e8205027fa3dc7ff0abd08bbd8d2128ebdbb0e4
Jim Vacca <33520581+MSFTJim@users.noreply.github.com>
2024-05-31 14:31:54 +0000

    refactored endpoints

--- apiStorage/Program.cs
+++ apiStorage/Program.cs
@@ -4,37 +4,46 @@ using Azure.Storage.Blobs.Models;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/SecurityTimes/{airportCode}", async (string airportCode) =>
+{
+    string jsonFileSuffix = "SecurityTimes.json";
+    string blobContents = await GetBlobContents(airportCode, jsonFileSuffix);
+    return blobContents;
+});
 
-app.MapGet("/getJSON/{airportCode}", async (string airportCode) =>
+app.MapGet("/WalkTimes/{airportCode}", async (string airportCode) =>
+{
+    string jsonFileSuffix = "WalkTimes.json";
+    string blobContents = await GetBlobContents(airportCode, jsonFileSuffix);
+    return blobContents;
+});
+
+async Task<string> GetBlobContents(string airportCode, string jsonFileSuffix)
 {
     // retreive Azure Storage items from settings (in development, keys in secrets.json )
     string? connectionString = app.Configuration["AzureBlobStorage:ConnectionString"];
     string? containerName = app.Configuration["AzureBlobStorage:ContainerName"];
     string upperAirportCode = airportCode.ToUpper();
-    
 
     // Create a BlobServiceClient
     BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
 
     // Create a BlobContainerClient
-    BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);    
-    
+    BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+
     // Create a BlobClient
-    string jsonFileName = $"{airportCode.ToUpper()}SecurityTimes.json";
+    string jsonFileName = upperAirportCode + jsonFileSuffix;
     BlobClient blobClient = containerClient.GetBlobClient(jsonFileName);
 
     // Download the blob content
     BlobDownloadResult downloadResult = await blobClient.DownloadContentAsync();
     string blobContents = downloadResult.Content.ToString();
-    
-    app.Logger.LogInformation($"jsonFileName: {jsonFileName}");
+
+    //app.Logger.LogInformation($"jsonFileName: {jsonFileName}");
     app.Logger.LogInformation($"blobContents: {blobContents}");
 
     return blobContents;
-
-});
-
+}
 
 
 app.Run();
